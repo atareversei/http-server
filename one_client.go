ac1d67b9f2762c50ed6fa072d6526f7bff69c71f
atareversei (vivobook) <atareversei@gmail.com>
2025-01-16 17:22:29 +0330

    finish project one

--- one_client.go
+++ one_client.go
@@ -1,22 +1,117 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"github.com/atareversei/network-course-projects/pkg/cli"
+	"github.com/atareversei/network-course-projects/pkg/colorize"
 	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
+var clientKeys = make(map[int]int)
+
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
+	fmt.Printf(
+		"made in %s",
+		colorize.
+			New("basliq labs\n").
+			Modify(colorize.BrightBlue).
+			Commit())
+
+	hostFlag := flag.String("host", "localhost", "Host to dial")
+	portFlag := flag.Int("port", 8080, "Port to dial")
+	flag.Parse()
+
+	host := *hostFlag
+	port := *portFlag
+
+	data, err := os.ReadFile("./one_key.txt")
 	if err != nil {
-		fmt.Println(err)
-		return
+		cli.Error("server could not find the keys", err)
+		os.Exit(1)
 	}
 
-	_, err = conn.Write([]byte("Hello, server!"))
-	if err != nil {
-		fmt.Println(err)
-		return
+	for _, key := range strings.Split(strings.ReplaceAll(string(data), "\r", ""), "\n") {
+		index, err := strconv.Atoi(strings.Trim(strings.Split(key, ":")[0], " "))
+		if err != nil {
+			cli.Error("cannot read key index", err)
+			continue
+		}
+
+		value, err := strconv.Atoi(strings.Trim(strings.Split(key, ":")[1], " "))
+		if err != nil {
+			cli.Error("cannot read key value", err)
+			continue
+		}
+		clientKeys[index] = value
 	}
 
-	conn.Close()
+	for {
+		fmt.Println("1. dial\n2. exit")
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			cli.Error("invalid input", err)
+		}
+		input = strings.TrimSuffix(input, "\r\n")
+		fmt.Println([]byte(input), []byte("dial"))
+		if input == "dial" || input == "1" {
+			Dial(host, port)
+		}
+		if input == "exit" || input == "2" {
+			cli.Info("exiting the program")
+			os.Exit(1)
+		}
+	}
+}
+
+func Dial(host string, port int) {
+	cli.Info(fmt.Sprintf("dialing server at: %s:%d", host, port))
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		cli.Error("couldn't connect to the server", err)
+		return
+	}
+	defer conn.Close()
+	for {
+		fmt.Println("Write a message to be delivered to server or close the connection with `close`:")
+		r := bufio.NewReader(os.Stdin)
+		input, err := r.ReadString('\n')
+		if err != nil {
+			cli.Error("couldn't read from Stdin, skipping this iteration", err)
+			continue
+		}
+		input = strings.TrimSuffix(input, "\r\n")
+		if input == "close" {
+			cli.Info("closing the connection")
+			break
+		}
+		_, err = conn.Write([]byte(input + "\n"))
+		if err != nil {
+			cli.Error("couldn't send the data to server, skipping this iteration", err)
+			continue
+		}
+		resReader := bufio.NewReader(conn)
+		res, err := resReader.ReadString('\n')
+		if err != nil {
+			cli.Error("couldn't read the response", err)
+		}
+		res = strings.TrimSuffix(res, "\n")
+		dashIndex := strings.Index(res, "-")
+		index, err := strconv.Atoi(res[0:dashIndex])
+		if err != nil {
+			cli.Error(fmt.Sprintf("malformed index: %s", res[0:dashIndex]), err)
+			continue
+		}
+		key := clientKeys[index]
+		var builder strings.Builder
+		for _, ch := range res[dashIndex+1:] {
+			builder.WriteRune(ch - int32(key))
+		}
+		cli.Success(fmt.Sprintf("encryption was successful:\n\t\t\tresponse: %s\n\t\t\tdecrypted message: %s\n\t\t\tindex used: %d\n\t\t\tkey used: %d", res, builder.String(), index, key))
+	}
 }
--- one_server.go
+++ one_server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/atareversei/network-course-projects/pkg/cli"
 	"github.com/atareversei/network-course-projects/pkg/colorize"
+	"math/rand"
 	"net"
 	"os"
 	"strconv"
@@ -21,8 +23,10 @@ func main() {
 			Modify(colorize.BrightBlue).
 			Commit())
 
-	port := flag.Int("port", 8080, "port number to spawn the server process")
+	portFlag := flag.Int("port", 8080, "port number to spawn the server process")
 	flag.Parse()
+
+	port := *portFlag
 
 	data, err := os.ReadFile("./one_key.txt")
 	if err != nil {
@@ -45,14 +49,14 @@ func main() {
 		keys[index] = value
 	}
 
-	cli.Success(fmt.Sprintf("tcp server started at :%d", *port))
-	skt, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	cli.Success(fmt.Sprintf("tcp server started at :%d", port))
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		cli.Error("server could not be started", err)
 	}
 
 	for {
-		conn, err := skt.Accept()
+		conn, err := l.Accept()
 		if err != nil {
 			cli.Error("connection resulted in an error", err)
 			continue
@@ -64,12 +68,25 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		cli.Error("could not read the content", err)
-		return
+	r := bufio.NewReader(conn)
+	for {
+		message, err := r.ReadString('\n')
+		if err != nil {
+			cli.Info(fmt.Sprintf("client (%s) closed the connection", conn.RemoteAddr()))
+			break
+		}
+		message = strings.TrimSuffix(message, "\n")
+		cli.Info(fmt.Sprintf("recieved message: %s", message))
+		ucm := strings.ToUpper(message)
+		index := rand.Intn(10)
+		key := keys[index]
+
+		var builder strings.Builder
+		for _, ch := range ucm {
+			builder.WriteRune(ch + int32(key))
+		}
+		response := fmt.Sprintf("%d-%s\n", index, builder.String())
+		cli.Success(fmt.Sprintf("encryption was successful:\n\t\t\tmessage: %s\n\t\t\tencrypted message: %s\n\t\t\tindex used: %d\n\t\t\tkey used: %d", message, builder.String(), index, key))
+		conn.Write([]byte(response))
 	}
-	r := string(buf)
-	cli.Info(fmt.Sprintf("recieved bytes: %v", r))
 }
